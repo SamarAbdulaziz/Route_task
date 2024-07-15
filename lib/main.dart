e7947d80f6aa048e8f3a3c74ca74bb2efb9f9632
SamarAbdulaziz <sam_abdulaziz@yahoo.com>
2024-07-15 06:16:36 +0300

    state is success

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:route_tech_summit_task/core/utils/api_service.dart';
 import 'package:route_tech_summit_task/core/utils/service_locator/service_locator.dart';
 import 'package:route_tech_summit_task/features/home/data/repos/home_repo.dart';
-import 'package:route_tech_summit_task/features/home/data/repos/home_repo_impl.dart';
 import 'package:route_tech_summit_task/features/home/presentation/view_model/product_cubit/product_cubit.dart';
 import 'package:route_tech_summit_task/features/home/presentation/views/home_view.dart';
 
